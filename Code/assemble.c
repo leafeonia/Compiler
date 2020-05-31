7df17449648f337ec277bc753bd7d2df24ad99c8
171860575-Mao Yiming <maoym@126.com>
2020-06-01 01:11:32 +0800

    assemble translation without function call

--- Code/assemble.c
+++ Code/assemble.c
@@ -1,10 +1,11 @@
 #include "assemble.h"
+#include "ir.h"
 #include <stdio.h>
 
 extern InterCode* irHead;
 FILE* fp;
 char* regs[] = {"$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "$t9"};
-int index = 0;
+int idx = 0;
 
 void printAssemble(FILE* file){
 	fp = file;
@@ -16,7 +17,7 @@ void printAssemble(FILE* file){
 }
 
 char* getReg(Operand* op){
-	return regs[(index++) % 10];
+	return regs[(idx++) % 10];
 }
 
 void irToAssemble(InterCode* ir){
@@ -25,17 +26,34 @@ void irToAssemble(InterCode* ir){
 			break;
 		case I_PARAM:
 			break;
-		case I_ASSIGN:
-			break;
+		case I_ASSIGN: {
+		    Operand* left = ir->u.assign.left;
+		    Operand* right = ir->u.assign.right;
+		    if (right->kind == OP_NUM){
+		        fprintf(fp, "li %s %d\n", getReg(left), right->u.var_no);
+		    }
+		    else{
+		        fprintf(fp, "move %s %s\n", getReg(left), getReg(right));
+		    }
+            break;
+        }
 		case I_READ:
+		    fprintf(fp, "addi $sp $sp -4\nsw $ra, 0($sp)\njal read\nlw $ra, 0($sp)\naddi $sp, $sp, 4\n");
+		    fprintf(fp, "move %s $v0\n", getReg(ir->u.singleOp));
 			break;
 		case I_WRITE:
-			break;
+		    fprintf(fp, "move $a0 %s\n", getReg(ir->u.singleOp));
+            fprintf(fp, "addi $sp $sp -4\nsw $ra, 0($sp)\njal write\nlw $ra, 0($sp)\naddi $sp, $sp, 4\n");
+            break;
 		case I_CALL:
+		    fprintf(fp, "jal %s\n", ir->u.assign.right->u.value);
+		    fprintf(fp, "move %s $v0\n", getReg(ir->u.assign.left));
 			break;
 		case I_ARG:
 			break;
 		case I_RETURN:
+		    fprintf(fp, "move $v0, %s\n", getReg(ir->u.singleOp));
+		    fprintf(fp, "jr $ra\n");
 			break;
 		case I_LABEL:
 			fprintf(fp, "label%d:\n", ir->u.singleOp->u.var_no);
@@ -43,8 +61,17 @@ void irToAssemble(InterCode* ir){
 	    case I_GOTO:
 	    	fprintf(fp, "j label%d\n", ir->u.singleOp->u.var_no);
 	    	break;
-	    case I_IFGOTO:
-	    	break;
+	    case I_IFGOTO: {
+            char* val = ir->u.triop.relop->u.value;
+            if (!strcmp(val, "<")) fprintf(fp, "blt");
+            else if (!strcmp(val, ">")) fprintf(fp, "bgt");
+            else if (!strcmp(val, "==")) fprintf(fp, "beq");
+            else if (!strcmp(val, "!=")) fprintf(fp, "bne");
+            else if (!strcmp(val, ">=")) fprintf(fp, "bge");
+            else if (!strcmp(val, "<=")) fprintf(fp, "ble");
+            fprintf(fp, " %s %s label%d\n", getReg(ir->u.triop.op1), getReg(ir->u.triop.op2), ir->u.triop.op3->u.var_no);
+            break;
+        }
 	    case I_ADD:{
 	    	Operand* result = ir->u.binop.result;
 	    	Operand* op1 = ir->u.binop.op1;
@@ -52,15 +79,31 @@ void irToAssemble(InterCode* ir){
 	    	if(ir->u.binop.op2->kind == OP_NUM){
 	    		fprintf(fp, "addi %s, %s, %d\n", getReg(result), getReg(op1), op2->u.var_no);
 	    	}
+	    	else{
+	    	    fprintf(fp, "add %s, %s, %s\n", getReg(result), getReg(op1), getReg(op2));
+	    	}
 	    	break;
 	    }
-	    case I_MINUS:
-	    	break;
+	    case I_MINUS: {
+            Operand *result = ir->u.binop.result;
+            Operand *op1 = ir->u.binop.op1;
+            Operand *op2 = ir->u.binop.op2;
+            if (ir->u.binop.op2->kind == OP_NUM) {
+                fprintf(fp, "addi %s, %s, %d\n", getReg(result), getReg(op1), -op2->u.var_no);
+            } else {
+                fprintf(fp, "sub %s, %s, %s\n", getReg(result), getReg(op1), getReg(op2));
+            }
+            break;
+        }
 	    case I_STAR:
+	        fprintf(fp, "mul %s, %s, %s\n", getReg(ir->u.binop.result), getReg(ir->u.binop.op1), getReg(ir->u.binop.op2));
 	    	break;
 	    case I_DIV:
+            fprintf(fp, "div %s, %s\n", getReg(ir->u.binop.op1), getReg(ir->u.binop.op2));
+            fprintf(fp, "mflo, %s\n", getReg(ir->u.binop.result));
 	    	break;
 	    case I_DEC:
+	        fprintf(fp, "TODO DEC\n");
 	    	break;
 	    default:
 	    	break;
