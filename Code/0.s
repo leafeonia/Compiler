1d1c5defbd8daae097ff20b9a2e497c14a15a6e5
171860575-Mao Yiming <maoym@126.com>
2020-06-07 23:37:08 +0800

    stack management

--- Code/0.s
+++ Code/0.s
@@ -19,8 +19,289 @@ write:
   syscall
   move $v0, $0
   jr $ra
-label0:
-j label2
-label1:
-j label0
-label2:
+f_add:
+sw $sp, 0($sp)
+addi $sp, $sp, -4
+sw $fp, 0($sp)
+addi $sp, $sp, -4
+move $fp, $sp
+sw $ra, 0($sp)
+lw $t0, 16($fp)
+sw $t0, -12($fp)
+addi $sp, $sp, -4
+lw $t0, 20($fp)
+sw $t0, -16($fp)
+addi $sp, $sp, -4
+lw $t1, -12($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+addi $sp, $sp, -4
+sw $t2, -20($fp)
+lw $t0, 24($fp)
+sw $t0, -24($fp)
+addi $sp, $sp, -4
+lw $t1, -20($fp)
+lw $t0, -24($fp)
+add $t2, $t1, $t0
+addi $sp, $sp, -4
+sw $t2, -28($fp)
+lw $t0, 28($fp)
+sw $t0, -32($fp)
+addi $sp, $sp, -4
+lw $t1, -28($fp)
+lw $t0, -32($fp)
+add $t2, $t1, $t0
+addi $sp, $sp, -4
+sw $t2, -36($fp)
+lw $t0, 32($fp)
+sw $t0, -40($fp)
+addi $sp, $sp, -4
+lw $t1, -36($fp)
+lw $t0, -40($fp)
+add $t2, $t1, $t0
+addi $sp, $sp, -4
+sw $t2, -44($fp)
+lw $t0, -44($fp)
+sw $t0, -48($fp)
+addi $sp, $sp, -4
+lw $t0, -48($fp)
+sw $t0, -52($fp)
+addi $sp, $sp, -4
+lw $t0, -52($fp)
+move $a0, $t0
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+jal write
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+li $t0, 0
+sw $t0, -56($fp)
+addi $sp, $sp, -4
+lw $v0, -56($fp)
+lw $sp, 8($fp)
+lw $ra, 0($fp)
+lw $fp, 4($fp)
+jr $ra
+main:
+sw $sp, 0($sp)
+addi $sp, $sp, -4
+sw $fp, 0($sp)
+addi $sp, $sp, -4
+move $fp, $sp
+sw $ra, 0($sp)
+TODO DEC
+li $t0, 0
+sw $t0, -12($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -20($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -20($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -24($fp)
+lw $t1, -16($fp)
+lw $t0, -24($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+addi $sp, $sp, -4
+sw $t2, -28($fp)
+lw $t0, -12($fp)
+sw $t0, -32($fp)
+addi $sp, $sp, -4
+li $t0, 1
+sw $t0, -36($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 1
+sw $t0, -40($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -40($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -44($fp)
+lw $t1, -16($fp)
+lw $t0, -44($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -36($fp)
+sw $t0, -32($fp)
+li $t0, 2
+sw $t0, -48($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 2
+sw $t0, -52($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -52($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -56($fp)
+lw $t1, -16($fp)
+lw $t0, -56($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -48($fp)
+sw $t0, -32($fp)
+li $t0, 3
+sw $t0, -60($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 3
+sw $t0, -64($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -64($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -68($fp)
+lw $t1, -16($fp)
+lw $t0, -68($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -60($fp)
+sw $t0, -32($fp)
+li $t0, 4
+sw $t0, -72($fp)
+addi $sp, $sp, -4
+lw $t0, -72($fp)
+sw $t0, 20($fp)
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 0
+sw $t0, -76($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -76($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -80($fp)
+lw $t1, -16($fp)
+lw $t0, -80($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -28($fp)
+sw $t0, -84($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 1
+sw $t0, -88($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -88($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -92($fp)
+lw $t1, -16($fp)
+lw $t0, -92($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -28($fp)
+sw $t0, -96($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 2
+sw $t0, -100($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -100($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -104($fp)
+lw $t1, -16($fp)
+lw $t0, -104($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -28($fp)
+sw $t0, -108($fp)
+addi $sp, $sp, -4
+li $t0, 0
+sw $t0, -16($fp)
+li $t0, 3
+sw $t0, -112($fp)
+addi $sp, $sp, -4
+li $t0, #4
+lw $t1, -112($fp)
+addi $sp, $sp, -4
+mul $t2, $t1, $t0
+sw $t2, -116($fp)
+lw $t1, -16($fp)
+lw $t0, -116($fp)
+add $t2, $t1, $t0
+sw $t2, -16($fp)
+lw $t1, 16($fp)
+lw $t0, -16($fp)
+add $t2, $t1, $t0
+sw $t2, -28($fp)
+lw $t0, -28($fp)
+sw $t0, -120($fp)
+addi $sp, $sp, -4
+lw $t0, 20($fp)
+sw $t0, -124($fp)
+addi $sp, $sp, -4
+lw $t0, -124($fp)
+sw $t0, 0($sp)
+addi $sp, $sp, -4
+lw $t0, -120($fp)
+sw $t0, 0($sp)
+addi $sp, $sp, -4
+lw $t0, -108($fp)
+sw $t0, 0($sp)
+addi $sp, $sp, -4
+lw $t0, -96($fp)
+sw $t0, 0($sp)
+addi $sp, $sp, -4
+lw $t0, -84($fp)
+sw $t0, 0($sp)
+addi $sp, $sp, -4
+addi $sp, $sp, -4
+jal f_add
+move $t0, $v0
+addi $sp, $sp, -4
+sw $t0, -152($fp)
+li $t0, 0
+sw $t0, -156($fp)
+addi $sp, $sp, -4
+lw $v0, -156($fp)
+lw $sp, 8($fp)
+lw $ra, 0($fp)
+lw $fp, 4($fp)
+jr $ra
--- Code/assemble.c
+++ Code/assemble.c
@@ -1,58 +1,139 @@
 #include "assemble.h"
 #include "ir.h"
 #include <stdio.h>
+#include "hashTable.h"
 
 extern InterCode* irHead;
 FILE* fp;
-char* regs[] = {"$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "$t9"};
-int idx = 0;
+int offset = -4;
+int paramCount = 0;
+#define PUSH_SP {fprintf(fp, "addi $sp, $sp, -4\n");offset -= 4;}
+
+Value* setValOffset(int off){
+    Value* value = (Value*)malloc(sizeof(Value));
+    value->depth = 0;
+    int* ptr = (int*)malloc(sizeof(int));
+    *ptr = off;
+    value->val = (void*)ptr;
+    return value;
+}
+
+char* getOpName(Operand* op){
+    char* ret;
+    if (op->kind == OP_TEMPVAR){
+        ret = (char*)malloc(12);
+        sprintf(ret, "t%d", op->u.var_no);
+    }
+    else{
+        ret = (char*)malloc(64);
+        sprintf(ret, "%s", op->u.value);
+    }
+    return ret;
+}
+
+int getValOffset(Operand* op){
+    Value* value = hashRead(getOpName(op));
+    assert(value);
+    return *(int*)value->val;
+}
 
 void printAssemble(FILE* file){
 	fp = file;
 	fprintf(fp, ".data\n_prompt: .asciiz \"Enter an integer:\"\n_ret: .asciiz \"\\n\"\n.globl main\n.text\nread:\n  li $v0, 4\n  la $a0, _prompt\n  syscall\n  li $v0, 5\n  syscall\n  jr $ra\n\nwrite:\n  li $v0, 1\n  syscall\n  li $v0, 4\n  la $a0, _ret\n  syscall\n  move $v0, $0\n  jr $ra\n");
+	hashInit();
 	while(irHead){
 		irToAssemble(irHead);
 		irHead = irHead->next;
 	}
 }
 
-char* getReg(Operand* op){
-	return regs[(idx++) % 10];
-}
-
 void irToAssemble(InterCode* ir){
 	switch (ir->kind){
-		case I_FUNCTION:
-			break;
+		case I_FUNCTION: {
+            paramCount = 0;
+            offset = -4;
+            char *funcName = ir->u.singleOp->u.value;
+            if (!strcmp(funcName, "main")) fprintf(fp, "main:\n");
+            else fprintf(fp, "f_%s:\n", funcName);
+
+            //save $sp and $fp
+            fprintf(fp, "sw $sp, 0($sp)\n");
+            PUSH_SP;
+            fprintf(fp, "sw $fp, 0($sp)\n");
+            PUSH_SP;
+            fprintf(fp, "move $fp, $sp\nsw $ra, 0($sp)\n");
+            break;
+        }
 		case I_PARAM:
+		    paramCount++;
+		    hashInsert(getOpName(ir->u.singleOp), setValOffset(4 * paramCount + 12));
 			break;
 		case I_ASSIGN: {
 		    Operand* left = ir->u.assign.left;
 		    Operand* right = ir->u.assign.right;
 		    if (right->kind == OP_NUM){
-		        fprintf(fp, "li %s %d\n", getReg(left), right->u.var_no);
+		        fprintf(fp, "li $t0, %d\n", right->u.var_no);
+		        Value* val = hashRead(getOpName(left));
+		        if (val){
+		            fprintf(fp, "sw $t0, %d($fp)\n", getValOffset(left));
+		        }
+		        else{
+		            fprintf(fp, "sw $t0, %d($fp)\n", offset);
+		            hashInsert(getOpName(left), setValOffset(offset));
+		            PUSH_SP
+		        }
 		    }
 		    else{
-		        fprintf(fp, "move %s %s\n", getReg(left), getReg(right));
+		        fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(right));
+		        Value* val = hashRead(getOpName(left));
+		        if (val){
+		            fprintf(fp, "sw $t0, %d($fp)\n", getValOffset(left));
+		        }
+		        else{
+                    fprintf(fp, "sw $t0, %d($fp)\n", offset);
+                    hashInsert(getOpName(left), setValOffset(offset));
+                    PUSH_SP
+		        }
 		    }
             break;
         }
 		case I_READ:
-		    fprintf(fp, "addi $sp $sp -4\nsw $ra, 0($sp)\njal read\nlw $ra, 0($sp)\naddi $sp, $sp, 4\n");
-		    fprintf(fp, "move %s $v0\n", getReg(ir->u.singleOp));
+		    fprintf(fp, "addi $sp, $sp, -4\nsw $ra, 0($sp)\njal read\nlw $ra, 0($sp)\naddi $sp, $sp, 4\n");
+		    fprintf(fp, "move $t0, $v0\n");
+		    hashInsert(getOpName(ir->u.singleOp), setValOffset(offset));
+		    PUSH_SP
+		    fprintf(fp, "sw $t0, %d($fp)\n", offset + 4);
 			break;
 		case I_WRITE:
-		    fprintf(fp, "move $a0 %s\n", getReg(ir->u.singleOp));
-            fprintf(fp, "addi $sp $sp -4\nsw $ra, 0($sp)\njal write\nlw $ra, 0($sp)\naddi $sp, $sp, 4\n");
+		    fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(ir->u.singleOp));
+		    fprintf(fp, "move $a0, $t0\n");
+            fprintf(fp, "addi $sp, $sp, -4\nsw $ra, 0($sp)\njal write\nlw $ra, 0($sp)\naddi $sp, $sp, 4\n");
             break;
-		case I_CALL:
-		    fprintf(fp, "jal %s\n", ir->u.assign.right->u.value);
-		    fprintf(fp, "move %s $v0\n", getReg(ir->u.assign.left));
-			break;
-		case I_ARG:
-			break;
+		case I_CALL: {
+		    PUSH_SP
+            char *funcName = ir->u.assign.right->u.value;
+            if (!strcmp(funcName, "main")) fprintf(fp, "jal main\n");
+            else fprintf(fp, "jal f_%s\n", ir->u.assign.right->u.value);
+            fprintf(fp, "move $t0, $v0\n");
+            hashInsert(getOpName(ir->u.assign.left), setValOffset(offset));
+            PUSH_SP
+            fprintf(fp, "sw $t0, %d($fp)\n", getValOffset(ir->u.assign.left));
+            break;
+        }
+		case I_ARG: {
+            fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(ir->u.singleOp));
+            fprintf(fp, "sw $t0, 0($sp)\n");
+            PUSH_SP
+            break;
+        }
 		case I_RETURN:
-		    fprintf(fp, "move $v0, %s\n", getReg(ir->u.singleOp));
+		    fprintf(fp, "lw $v0, %d($fp)\n", getValOffset(ir->u.singleOp));
+
+		    //restore $sp and $fp
+		    fprintf(fp, "lw $sp, 8($fp)\n");
+            fprintf(fp, "lw $ra, 0($fp)\n");
+		    fprintf(fp, "lw $fp, 4($fp)\n");
+
 		    fprintf(fp, "jr $ra\n");
 			break;
 		case I_LABEL:
@@ -62,6 +143,8 @@ void irToAssemble(InterCode* ir){
 	    	fprintf(fp, "j label%d\n", ir->u.singleOp->u.var_no);
 	    	break;
 	    case I_IFGOTO: {
+            fprintf(fp, "lw $t1, %d($fp)\n", getValOffset(ir->u.triop.op1));
+            fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(ir->u.triop.op2));
             char* val = ir->u.triop.relop->u.value;
             if (!strcmp(val, "<")) fprintf(fp, "blt");
             else if (!strcmp(val, ">")) fprintf(fp, "bgt");
@@ -69,38 +152,74 @@ void irToAssemble(InterCode* ir){
             else if (!strcmp(val, "!=")) fprintf(fp, "bne");
             else if (!strcmp(val, ">=")) fprintf(fp, "bge");
             else if (!strcmp(val, "<=")) fprintf(fp, "ble");
-            fprintf(fp, " %s %s label%d\n", getReg(ir->u.triop.op1), getReg(ir->u.triop.op2), ir->u.triop.op3->u.var_no);
+            fprintf(fp, " $t1, $t0, label%d\n", ir->u.triop.op3->u.var_no);
             break;
         }
 	    case I_ADD:{
 	    	Operand* result = ir->u.binop.result;
 	    	Operand* op1 = ir->u.binop.op1;
 	    	Operand* op2 = ir->u.binop.op2;
-	    	if(ir->u.binop.op2->kind == OP_NUM){
-	    		fprintf(fp, "addi %s, %s, %d\n", getReg(result), getReg(op1), op2->u.var_no);
+	    	if(op2->kind == OP_NUM){
+	    	    fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(op1));
+	    		fprintf(fp, "addi $t2, $t0, %d\n", op2->u.var_no);
 	    	}
 	    	else{
-	    	    fprintf(fp, "add %s, %s, %s\n", getReg(result), getReg(op1), getReg(op2));
+	    	    fprintf(fp, "lw $t1, %d($fp)\n", getValOffset(op1));
+                fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(op2));
+	    	    fprintf(fp, "add $t2, $t1, $t0\n");
 	    	}
+            Value* value = hashRead(getOpName(result));
+            if (!value){
+                hashInsert(getOpName(result), setValOffset(offset));
+                PUSH_SP
+            }
+	    	fprintf(fp, "sw $t2, %d($fp)\n", getValOffset(result));
 	    	break;
 	    }
 	    case I_MINUS: {
+            Operand* result = ir->u.binop.result;
+            Operand* op1 = ir->u.binop.op1;
+            Operand* op2 = ir->u.binop.op2;
+            if(op2->kind == OP_NUM){
+                fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(op1));
+                fprintf(fp, "addi $t2, $t0, %d\n", -op2->u.var_no);
+            }
+            else{
+                fprintf(fp, "lw $t1, %d($fp)\n", getValOffset(op1));
+                fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(op2));
+                fprintf(fp, "sub $t2, $t1, $t0\n");
+            }
+            Value* value = hashRead(getOpName(result));
+            if (!value){
+                hashInsert(getOpName(result), setValOffset(offset));
+                PUSH_SP
+            }
+            fprintf(fp, "sw $t2, %d($fp)\n", getValOffset(result));
+            break;
+        }
+	    case I_STAR: {
             Operand *result = ir->u.binop.result;
             Operand *op1 = ir->u.binop.op1;
             Operand *op2 = ir->u.binop.op2;
-            if (ir->u.binop.op2->kind == OP_NUM) {
-                fprintf(fp, "addi %s, %s, %d\n", getReg(result), getReg(op1), -op2->u.var_no);
-            } else {
-                fprintf(fp, "sub %s, %s, %s\n", getReg(result), getReg(op1), getReg(op2));
+            if (op2->kind == OP_NUM){
+                fprintf(fp, "li $t0, #%d\n", op2->u.var_no);
             }
+            else {
+                fprintf(fp, "lw $t0, %d($fp)\n", getValOffset(op2));
+            }
+            fprintf(fp, "lw $t1, %d($fp)\n", getValOffset(op1));
+            if (!hashRead(getOpName(result))){
+                hashInsert(getOpName(result), setValOffset(offset));
+                PUSH_SP
+            }
+            fprintf(fp, "mul $t2, $t1, $t0\n");
+            fprintf(fp, "sw $t2, %d($fp)\n", getValOffset(result));
             break;
         }
-	    case I_STAR:
-	        fprintf(fp, "mul %s, %s, %s\n", getReg(ir->u.binop.result), getReg(ir->u.binop.op1), getReg(ir->u.binop.op2));
-	    	break;
 	    case I_DIV:
-            fprintf(fp, "div %s, %s\n", getReg(ir->u.binop.op1), getReg(ir->u.binop.op2));
-            fprintf(fp, "mflo, %s\n", getReg(ir->u.binop.result));
+//            fprintf(fp, "div %s, %s\n", getReg(ir->u.binop.op1), getReg(ir->u.binop.op2));
+//            fprintf(fp, "mflo, %s\n", getReg(ir->u.binop.result));
+            fprintf(fp, "TODO DIV\n");
 	    	break;
 	    case I_DEC:
 	        fprintf(fp, "TODO DEC\n");
